664cba264d6193dbb35e398aeaa917db10a34d6b
Jennifer Cwagenberg <candiedcode@gmail.com>
2026-02-18 20:47:26 -0600

    style: address final clang-tidy error

--- Homework_2/main.cpp
+++ Homework_2/main.cpp
@@ -1,11 +1,11 @@
-#include <cstdio>
+#include <iostream>
 #include <omp.h>
 
 auto main() -> int
 {
 #pragma omp parallel
     {
-        printf("Hello ECE 4122 6122\n");
+        std::cout << "Hello ECE 4122 6122\n";
     }
 
     return 0;
