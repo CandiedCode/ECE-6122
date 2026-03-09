b37422df00c23259057ea55d7e8a7ad5ed03a828
Jennifer Cwagenberg <candiedcode@gmail.com>
2026-03-09 00:48:50 -0500

    chore: more skaffolding

---
+++ Homework_3/src/model.cpp
@@ -0,0 +1,68 @@
+#include "model.h"
+#include "mesh.h"
+#include "shader.h"
+
+Model::Model(const std::string &path)
+{
+    Assimp::Importer importer;
+    const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenNormals);
+
+    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+    {
+        return;
+    }
+
+    directory = path.substr(0, path.find_last_of('/'));
+    ProcessNode(scene->mRootNode, scene);
+}
+
+void Model::ProcessNode(aiNode *node, const aiScene *scene)
+{
+    for (unsigned i = 0; i < node->mNumMeshes; ++i)
+    {
+        aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+        meshes.push_back(ProcessMesh(mesh, scene));
+    }
+
+    for (unsigned i = 0; i < node->mNumChildren; ++i)
+    {
+        ProcessNode(node->mChildren[i], scene);
+    }
+}
+
+Mesh Model::ProcessMesh(aiMesh *mesh, const aiScene *)
+{
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+
+    for (unsigned i = 0; i < mesh->mNumVertices; ++i)
+    {
+        Vertex v;
+        v.Position = {mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z};
+        v.Normal = {mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z};
+        if (mesh->mTextureCoords[0])
+            v.TexCoords = {mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y};
+        else
+            v.TexCoords = {0.0f, 0.0f};
+        vertices.push_back(v);
+    }
+
+    for (unsigned i = 0; i < mesh->mNumFaces; ++i)
+    {
+        aiFace face = mesh->mFaces[i];
+        for (unsigned j = 0; j < face.mNumIndices; ++j)
+        {
+            indices.push_back(face.mIndices[j]);
+        }
+    }
+
+    return Mesh(vertices, indices);
+}
+
+void Model::Draw(Shader &shader) const
+{
+    for (const auto &mesh : meshes)
+    {
+        mesh.Draw();
+    }
+}
